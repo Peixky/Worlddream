354237b30018352966199028828095d589a5b49f
Peixky <20050322lp@gmail.com>
2025-04-18 15:56:05 +0800

    Update PlayerMovement.cs

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -11,7 +11,20 @@ public class Playermovement : MonoBehaviour
     }
 
     private void Update(){
+        float  horizontalInput = Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);//在y值不變的情況下紀錄x
+        
+        //Flip player when moving left-right
+        if(horizontalInput > 0.01f){
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }else if(horizontalInput < -0.01f){
+            Vector3 scale = transform.localScale;
+            scale.x = horizontalInput > 0.01f ? Mathf.Abs(scale.x) : -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+
         if(Input.GetKey(KeyCode.Space)){
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
         }
