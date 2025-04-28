5c3c96766e340d4372cc6eecebe84ef12b4df7af
derick0515 <dericklien0515@gmail.com>
2025-04-29 02:55:37 +0800

    這個是開場檔案
    註冊了不同的指令來在對話中觸發一些事件。這些指令是：
    
    Load_Scene: 觸發後會加載指定的場景。
    
    Lock_Everything: 觸發後鎖定玩家控制，禁止玩家移動。
    
    Unlock_Everything: 觸發後解鎖玩家控制，允許玩家移動。
    
    Start_Introduction: 觸發後顯示或啟動指定物體的介紹，通常是顯示一個動畫或界面。
    
    End_Introduction: 觸發後結束介紹並將某些狀態設置為完成。
    
    Start_Dad: 觸發後開始計時並改變相機位置，這可能是某種遊戲事件的開始。
    
    Final: 觸發後顯示最終提示或某個結局界面。
    
    更新函數：
    在 Update() 中，每當玩家按下任何按鍵時，會調用 flowerSystem.Next()，這會顯示下一段對話或文字內容。

---
+++ Assets/Levels/開場.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Flower;
+
+public class Pre_Introduction_Control : MonoBehaviour
+{
+    public FlowerSystem flowerSystem;  // 使用 public
+    public const string defaultSceneName = "default";  // 使用 public const 
+
+    // 初始化 FlowerSystem 並設置指令
+    void Start()
+    {
+        InitializeFlowerSystem();
+    }
+
+    // 每次按鍵時進行對話進行
+    void Update()
+    {
+        HandleInput();
+    }
+
+    // 初始化 FlowerSystem 並設置必要的對話和指令
+    public void InitializeFlowerSystem()
+    {
+        flowerSystem = FlowerManager.Instance.CreateFlowerSystem(defaultSceneName, false);
+        flowerSystem.SetupDialog();
+        flowerSystem.ReadTextFromResource("Pre_Introduction");
+
+        RegisterCommands();
+    }
+
+    // 註冊指令，這些指令會在對話中觸發
+    public void RegisterCommands()
+    {
+        flowerSystem.RegisterCommand("Load_Scene", (List<string> _params) => LoadScene(_params[0]));
+        flowerSystem.RegisterCommand("Lock_Everything", (List<string> _params) => LockPlayer(true));
+        flowerSystem.RegisterCommand("Unlock_Everything", (List<string> _params) => LockPlayer(false));
+        flowerSystem.RegisterCommand("Start_Introduction", (List<string> _params) => StartIntroduction(_params[0]));
+        flowerSystem.RegisterCommand("End_Introduction", (List<string> _params) => EndIntroduction());
+        flowerSystem.RegisterCommand("Start_Dad", (List<string> _params) => StartDad());
+        flowerSystem.RegisterCommand("Final", (List<string> _params) => ShowFinalHint());
+    }
+
+    // 處理用戶輸入，顯示下一段對話
+    public void HandleInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            flowerSystem.Next();
+        }
+    }
+
+    // 加載場景
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // 鎖定或解鎖玩家
+    public void LockPlayer(bool isLocked)
+    {
+        Player_Control.is_lock = isLocked;
+    }
+
+    // 開始介紹
+    public void StartIntroduction(string introObjectName)
+    {
+        GameObject.Find(introObjectName).GetComponent<Intro_System_Control>().intro.SetActive(true);
+    }
+
+    // 結束介紹
+    public void EndIntroduction()
+    {
+        Intro_System_Control.show_intro = true;
+    }
+
+    // 開始父親相關事件
+    public void StartDad()
+    {
+        player1_dad.timer_dad = 5;
+        Camera.main.transform.position = new Vector3(-70.0f, -22.86f, -10.0f);
+    }
+
+    // 顯示最終提示
+    public void ShowFinalHint()
+    {
+        GameObject.Find("System").GetComponent<Final_Hint>().final_hint.SetActive(true);
+    }
+}
