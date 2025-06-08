bf56c8624fb5fa4575d5607357e9ee0da0e9dbd5
chihhan28 <chihhan0208@gmail.com>
2025-06-08 16:21:01 +0800

    再改

--- Assets/Scripts/Level2/Enemy/RangeChaser/RangedChaserBullet.cs
+++ Assets/Scripts/Level2/Enemy/RangeChaser/RangedChaserBullet.cs
@@ -5,16 +5,24 @@ public class RangedChaserBullet : MonoBehaviour
     public int damage = 1;
     public float lifetime = 5f;
 
+    [Header("旋轉設定")]
+    public float rotationSpeed = 360f; // 每秒旋轉角度
+
     void Start()
     {
         Destroy(gameObject, lifetime); // 時間到自動刪除
+    }
+
+    void Update()
+    {
+        // 讓子彈持續旋轉
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-
             Destroy(gameObject); // 撞到玩家後刪除
         }
         else if (!other.CompareTag("Enemy"))
